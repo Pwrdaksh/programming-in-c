36b14e2e765b682aa292078e0ac5ecd727a565a8
DAKSH PAWAR <124837385+Pwrdaksh@users.noreply.github.com>
2025-08-31 23:45:37 +0530

    Add files via upload

---
+++ experiment 1.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+printf("Name – daksh pawar\nSAP ID - 590028473\nCourse - BCA\nBatch - B6");
+printf("\n--------------------------------------------");
+printf("\nHello World");
+printf("\n--------------------------------------------");
+getch();
+return 0;
+}
